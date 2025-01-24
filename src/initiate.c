68bb18c1bd4d1fe1abdb8ac4000d90648ef71696
Vladimir Lopatinski <vlopatin@c1r3p4.hive.fi>
2025-01-24 15:20:02 +0200

    cleaned pushswap and worked on fdf

--- src/initiate.c
+++ src/initiate.c
@@ -6,42 +6,25 @@
 /*   By: vlopatin <vlopatin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 17:48:30 by vlopatin          #+#    #+#             */
-/*   Updated: 2025/01/23 11:21:50 by vlopatin         ###   ########.fr       */
+/*   Updated: 2025/01/24 14:30:12 by vlopatin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fdf.h"
+#include "../include/fdf.h"
 
-// void	set_elevation(t_map *map)
-// {
-// 	int x;
-// 	int y;
-// 	int i;
-// 	int bigger_side;
-
-// 	y = -1;
-// 	i = 0;
-// 	if (map->width > map->height)
-// 		bigger_side = map->width;
-// 	else
-// 		bigger_side = map->height;
-// 	map->points[0].huge = 0;
-// 	while (++y < map->height)
-// 	{
-// 		x = -1;
-// 		while (++x < map->width)
-// 		{
-// 			// printf("ratio: %f\n", map->points[i].z / bigger_side);
-// 			if (map->points[i].z < 0)
-// 				map->points[i].z *= -1;
-// 			if (map->points[i].z / bigger_side > 3)
-// 			{
-// 				map->points[0].huge = 1;
-// 			}
-// 			i++;
-// 		}
-// 	}
-// }
-
-// 0xAABBGGRR
+void	initiate_values_map(t_map *map)
+{
+	map->angle_x = 0;
+	map->angle_y = 0;
+	map->angle_z = 0;
+	map->z_scale = 1;
+	map->cl.non_elevated = 0xFF240F4B;
+	map->cl.elevated = 0xFF2E3D0B;
+	map->cl.background = 0xFF67C0FF;
+	map->x_offset = 0;
+	map->y_offset = 0;
+	map->zoom = 1;
+	map->original = NULL;
+	map->new2d = NULL;
+}
 
